d50ba4270c2694fb75cd20774a3eccbf5a4b1435
Vince Sangkhae <vince.sangkhae@sigfig.com>
2020-01-15 22:05:08 -0800

    Add FilmController

--- app/Http/Controllers/FilmController.php
+++ app/Http/Controllers/FilmController.php
@@ -10,33 +10,44 @@ class FilmController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function index()
     {
-        //
+        return response()->json(Film::all()->toArray());
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
-        //
+        $film = Film::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'release_year' => $request->release_year,
+            'length' => $request->length
+        ]);
+
+        return response()->json([
+            'status' => (bool)$film,
+            'data' => $film,
+            'message' => $film ? 'Film Created' : 'Error creating film'
+        ]);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Film  $film
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function show(Film $film)
     {
-        //
+        return response()->json($film);
     }
 
     /**
@@ -44,21 +55,32 @@ class FilmController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Film  $film
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function update(Request $request, Film $film)
     {
-        //
+        $status = $film->update(
+            $request->only(['title', 'description', 'release_year', 'length'])
+        );
+        return response()->json([
+            'status' => $status,
+            'message' => $status ? 'Film Updated!' : 'Error Updating Film'
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Film  $film
-     * @return \Illuminate\Http\Response
+     * @param \App\Film $film
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
      */
     public function destroy(Film $film)
     {
-        //
+        $status = $film->delete();
+        return response()->json([
+            'status' => $status,
+            'message' => $status ? 'Film Deleted!' : 'Error Deleting Film'
+        ]);
     }
 }
