02c375b890a625753da75389a36ec48ec019c434
Vince Sangkhae <vince.sangkhaae@sigfig.com>
2020-02-11 10:49:45 -0800

    Add dummy image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,6 +17,16 @@
                     You are logged in!
                 </div>
             </div>
+
+            <h2 style="margin-top: 40px;">Rentals</h2>
+            <div class="card" style="width: 300px;">
+                <img src="{{asset('/images/rental-default.jpg')}}" class="card-img-top" alt="">
+                <div class="card-body">
+                    <h5 class="card-title">Movie Title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
         </div>
     </div>
 </div>
